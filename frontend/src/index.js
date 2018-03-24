8f7239a51f1dbe07905a5b0e9362b1cd34114efa
Mephestys <mephestys@gmail.com>
2018-03-23 19:40:01 -0600

    Main index with expected routes and component imports. - Also creates Redux store.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,38 @@
+/* Libraries */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/* Components */
+import App from './App';
+import SignIn from './components/signin';
+import SignUp from './components/signup';
+import RequireAuth from './components/HOC/RequireAuth';
+import Jokes from './components/jokes';
+import SignOut from './components/signout';
+
+import reducers from './reducers';
+
+/* Styles */
+import './index.css';
+
+/* End Imports */
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={App} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/jokes' component={RequireAuth(Jokes)} />
+        <Route path='/signout' component={SignOut} />
+      </div>
+    </BrowserRouter>
+  </Provider>, 
+  document.getElementById('root')
+);
